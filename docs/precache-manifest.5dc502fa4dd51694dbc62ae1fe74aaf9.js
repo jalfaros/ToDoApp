0158ab21c249b30915becf3dd1e6f6739967e3c7
Jose Ignacio Alfaro <ignacioalfaro99@gmail.com>
2020-08-20 19:01:04 -0600

    Creando Docs

---
+++ docs/precache-manifest.5dc502fa4dd51694dbc62ae1fe74aaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadacea624508ea2326d2e14e2858798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc2d29624c94644fbd51",
+    "url": "/static/css/main.ce66876f.chunk.css"
+  },
+  {
+    "revision": "96e1a073b79591393456",
+    "url": "/static/js/2.ae0c08b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae0c08b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2d29624c94644fbd51",
+    "url": "/static/js/main.36076ff7.chunk.js"
+  },
+  {
+    "revision": "c2a4463da067d7ee0a4c",
+    "url": "/static/js/runtime-main.f3c16400.js"
+  }
+]);
